a9e5b4a49532bdebe0b4dbf7ed3a907b01b505fc
Darthex <mrharshulsharma@gmail.com>
2024-03-20 00:05:54 +0530

    updated api provider

--- lib/providers/api_provider.dart
+++ lib/providers/api_provider.dart
@@ -3,10 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class ApiNotifier extends StateNotifier<String> {
   ApiNotifier() : super('');
 
-// void addPlace(String title, File image) {
-//   final newPlace = PlaceModel(title: title, image: image);
-//   state = [newPlace, ...state];
-// }
   void addApi(String api) {
     state = api.toString();
   }
